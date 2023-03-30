2b33f59b1f29de3c5ef07c0e0d4420088fc76b7c
navis23 <napisszarkasy@gmail.com>
2023-03-30 21:20:19 +0700

    update head n title app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ export default defineNuxtConfig({
       shim: false
     },
     app: {
+      head: {
+        charset: 'utf-16',
+        viewport: 'width=device-width, initial-scale=1',
+        title: 'E-Inventory Permata Motor',
+        meta: [
+          // <meta name="description" content="My amazing site">
+          { name: 'description', content: 'E-Inventory system for Permata Motor Based on Gresik' }
+        ],
+      },
       pageTransition: { name: 'slide-fade', mode: 'out-in' }
     },
     modules: [
